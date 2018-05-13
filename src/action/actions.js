b8881cc6b52217ee397f84e38adfaadb474a980b
Xavyr Moss <xmoss10@gmail.com>
2018-05-12 22:29:20 -0700

    removed unwanted comments

--- src/action/actions.js
+++ src/action/actions.js
@@ -1,15 +1,5 @@
 import * as types from '../constants/actionTypes.js'
 
-//this is where things get a little gnarly.
-//every action needs to return an object to the reducer, (the action is accessible to the reducer through the dispatch())
-//the action's return object comes with an ActionType case statement (for the sake of the reducer's case statement), and
-//a payload- the data that will update the store.
-
-//Async Action
-//With Async redux activity, your action returns a function that resolves to a dispatched object
-//that has the async data. Here, getNasaDataAction is a function that returns the result of the promise that fetches
-//the nasa data. This fulfilled promise result is dispatched to the reducer after being passed to the payLoadForReducerFunction to be
-//organized into the standard object with type and payload (newNasaData).
 export const asyncAction = () => {
   return dispatch => {
     console.log('false => true in 3, 2, 1...');
@@ -19,10 +9,6 @@ export const asyncAction = () => {
   }
 };
 
-//Sync Action
-//Sync Action is much simpler- return the standard object that comes with the type property
-// (for the reducer's case statement),
-//and the payload that will be used to update the store.
 export const syncAction = () => {
   return {type: types.SYNC_ACTION, payload: true}
 }
