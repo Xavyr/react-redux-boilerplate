b09caec5eb0c20f495c4809532c8cbe78388495a
Xavyr Moss <xmoss10@gmail.com>
2018-09-30 22:44:16 -0700

    Chore(Refactor): Changed file directory, added test folders

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store.js';
-import App from './containers/Container.jsx';
-
+import App from './../src/containers/Container.jsx'
 ReactDOM.render(
   <Provider store={store}>
     <App />
